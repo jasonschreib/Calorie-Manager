1879ad762262ab4dd48a2b0dae716b519b2757d4
jasonschreib <jwschreiber1@gmail.com>
2021-12-22 15:23:26 -0800

    Created model and registered it

--- calorieManager/log/models.py
+++ calorieManager/log/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#model for entries
+class Entry(models.Model):
+  breakfast = models.IntegerField()
+  snackOne = models.IntegerField()
+  lunch = models.IntegerField()
+  snackTwo = models.IntegerField()
+  dinner = models.IntegerField()
+  caloriesBurned = models.IntegerField()
+  timestamp = models.DateTimeField(auto_now_add = True)
+  creator = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+
